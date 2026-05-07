423e9848d0f9327076d358ea099c2334c96250f7
JorgeYael99 <jorg.yael99@gmail.com>
2026-05-07 15:48:24 -0600

    fix: update

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -43,7 +43,7 @@ async def registro(request: Request, user: UserAuth):
             VALUES ($1, $2) RETURNING id
         """, user.username, hashed_password)
         
-        await conn.execute("INSERT INTO perfil_usuario (usuario_id) VALUES ($1)", user_id)
+        await conn.execute("INSERT INTO perfil_usuario (id, usuario_id) VALUES (DEFAULT, $1)", user_id)
         
         token = f"token_{user_id}_{user.username}"
         
